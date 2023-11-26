626b4af3ca7e73e8d908113ef6ba5a16a71c32be
Wyatt VO <votranquy96@gmail.com>
2023-11-26 23:58:14 +0700

    Refact to use composition API

--- src/views/AirportDetail.vue
+++ src/views/AirportDetail.vue
@@ -13,26 +13,33 @@
 <script>
 import TwoColumnLayout from '@/layouts/TwoColumnLayout.vue'
 import allAirports from '@/data/airports.js'
-import airportMixin from '@/mixins/airport.js'
+import { onMounted, ref } from 'vue'
+import { useRoute } from 'vue-router'
 
 export default {
   components: {
     TwoColumnLayout
   },
-  mixins: [ airportMixin ],
   data() {
     return {
       airport: ''
     }
   },
-  methods: {
-  getAirportByCode() {
-    return allAirports.filter(airport => airport.abbreviation === this.$route.params.code.toUpperCase())[0]
+  setup() {
+    const route = useRoute()
+    const airportWithCode = ref('')
+    function getAirportByCode() {
+      return allAirports.filter(airport => airport.abbreviation === route.params.code.toUpperCase())[0];
     }
-  },
-  mounted() {
-    this.airport = this.getAirportByCode()
-    this.getAirportWithCode(this.airport)
+    function getAirportWithCode(airport) {
+      return `${airport.name} - ${airport.abbreviation}`
+    }
+    onMounted(() => {
+      const airport = ref(getAirportByCode())
+      airportWithCode.value = getAirportWithCode(airport.value)
+
+    })
+    return { airportWithCode }
   }
 }
 </script>
